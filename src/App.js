54937187f92c3ebbe8a58d9941fbc8245c9b6da7
Ajai <ajaisajin06@gmail.com>
2021-05-10 14:10:25 +0530

    header-component

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component'
 
 
 function App() {
   return (
     <div>
+      
       <BrowserRouter>
-      <Switch>
-        <Route exact path='/' component = {HomePage} />
-        <Route path='/shop' component={ShopPage}/>
-      </Switch>
+        <Header />
+        <Switch>
+          <Route exact path='/' component = {HomePage} />
+          <Route path='/shop' component={ShopPage}/>
+        </Switch>
       </BrowserRouter>
       
     </div>
