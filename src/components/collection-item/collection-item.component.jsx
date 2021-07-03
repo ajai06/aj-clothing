a44bfb880f6d3b484f20296ace3f9d8bc9e08d8f
Ajai <ajaisajin06@gmail.com>
2021-07-03 17:00:50 +0530

    cart-doup

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,16 +1,27 @@
 import react from 'react';
+import { connect } from 'react-redux';
+
+import { addItem } from '../../redux/cart/cart.actions';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './collection-item.styles.scss';
 
-const CollcetionItem = ({id, name, price, imageUrl}) =>(
+const CollcetionItem = ({item, addItem}) =>{
+    const { name, price, imageUrl } = item;
+    return(
     <div className="collection-item">
         <div className ="image" style ={{backgroundImage: `url(${imageUrl})`}} />
         <div className="collection-footer">
             <span className="name">{name}</span>
             <span className="price">{price}</span>
         </div>
+        <CustomButton inverted onClick={()=>addItem(item)}> Add to cart </CustomButton>
     </div>
-)
+)};
+
+const mapDispatchToProps = dispatch => ({
+    addItem : item => dispatch(addItem(item))
+})
 
 
-export default CollcetionItem;
+export default connect(null, mapDispatchToProps)(CollcetionItem);
